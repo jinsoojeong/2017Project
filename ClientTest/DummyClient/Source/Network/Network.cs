546ea14127e98e5ce89aee3580050a389d5904ad
jinsoojeong <man3273@daum.net>
2018-07-20 02:28:00 +0900

    source update

--- ClientTest/DummyClient/Source/Network/Network.cs
+++ ClientTest/DummyClient/Source/Network/Network.cs
@@ -25,6 +25,8 @@ namespace DummyClient
         private NET_STATE state_ = NET_STATE.START;
 
         private PacketProcess packetProcee_;
+        private long ping_ = 0;
+
         ~Network()
         {
             if (this.isConnected()) {
@@ -163,10 +165,12 @@ namespace DummyClient
 
         private void heartBeat()
         {
-            while (this.isConnected())
+            while (isConnected())
             {
                 PK_C_NOTIFY_HEARTBEAT heartBeatPacket = new PK_C_NOTIFY_HEARTBEAT();
                 sendPacket(heartBeatPacket);
+
+                ping_ = DateTime.Now.Ticks;
 
                 Thread.Sleep(1000);
             }
